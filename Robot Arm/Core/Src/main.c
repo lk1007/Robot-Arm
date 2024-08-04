d2180392c0cdcfa3d0aa67761d143a7b137c0c85
lk1006 <kainliam0@gmail.com>
2024-08-03 23:26:35 -0400

    all joints on arm work

--- Robot Arm/Core/Src/main.c
+++ Robot Arm/Core/Src/main.c
@@ -50,7 +50,7 @@ TIM_HandleTypeDef htim5;
 
 /* USER CODE BEGIN PV */
 
-float USB_RX_Buf[5] = {90,90,90,90,90};
+float USB_RX_Buf[5] = {90, 90, 90, 90, 90};
 
 /* USER CODE END PV */
 
@@ -61,22 +61,25 @@ static void MX_TIM3_Init(void);
 static void MX_TIM5_Init(void);
 /* USER CODE BEGIN PFP */
 
-  float byteArrayToFloat(const uint8_t *byteArray) {
-    // Assuming the byteArray is in little-endian format
-    uint32_t value;
-    memcpy(&value, byteArray, sizeof(uint32_t));
+float byteArrayToFloat(const uint8_t *byteArray)
+{
+  // Assuming the byteArray is in little-endian format
+  uint32_t value;
+  memcpy(&value, byteArray, sizeof(uint32_t));
 
-    // Convert the raw integer value to float
-    float result;
-    memcpy(&result, &value, sizeof(float));
+  // Convert the raw integer value to float
+  float result;
+  memcpy(&result, &value, sizeof(float));
 
-    return result;
+  return result;
+}
+void receiveAnglesUSB(uint8_t *buf)
+{
+  for (int i = 0; i < 5; i++)
+  {
+    USB_RX_Buf[i] = byteArrayToFloat(buf + sizeof(float) * i);
   }
-  void receiveAnglesUSB(uint8_t* buf){
-    for(int i = 0; i < 5; i++){
-      USB_RX_Buf[i] = byteArrayToFloat(buf + sizeof(float)*i);
-    }
-  } 
+}
 
 /* USER CODE END PFP */
 
@@ -86,9 +89,9 @@ static void MX_TIM5_Init(void);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -117,23 +120,23 @@ int main(void)
   MX_USB_DEVICE_Init();
   MX_TIM5_Init();
   /* USER CODE BEGIN 2 */
-  joint_t wrist = {.htimer = &htim3, .CCRReg = &(TIM3->CCR2), .channel = TIM_CHANNEL_2, .min_angle = 0, .max_angle = 100};
-  joint_t elbow = {.htimer = &htim5, .CCRReg = &(TIM5->CCR2), .channel = TIM_CHANNEL_2, .min_angle = 20, .max_angle = 100};
+  joint_t wrist = {.htimer = &htim3, .CCRReg = &(TIM3->CCR2), .channel = TIM_CHANNEL_2, .min_angle = 0, .max_angle = 180};
+  joint_t elbow = {.htimer = &htim5, .CCRReg = &(TIM5->CCR2), .channel = TIM_CHANNEL_2, .min_angle = 20, .max_angle = 180};
   joint_t base = {.htimer = &htim3, .CCRReg = &(TIM3->CCR4), .channel = TIM_CHANNEL_4, .min_angle = 20, .max_angle = 180};
-  joint_t hand = {.htimer = &htim3, .CCRReg = &(TIM3->CCR3), .channel = TIM_CHANNEL_3, .min_angle = 0, .max_angle = 140};
-  joint_t shoulder = {.htimer = &htim5, .CCRReg = &(TIM5->CCR1), .channel = TIM_CHANNEL_1, .min_angle = 20, .max_angle = 120};
+  joint_t hand = {.htimer = &htim3, .CCRReg = &(TIM3->CCR3), .channel = TIM_CHANNEL_3, .min_angle = 0, .max_angle = 180};
+  joint_t shoulder = {.htimer = &htim5, .CCRReg = &(TIM5->CCR1), .channel = TIM_CHANNEL_1, .min_angle = 20, .max_angle = 180};
 
-  arm_t arm = 
-  {
-    .base = &base,
-    .shoulder = &shoulder,
-    .elbow = &elbow,
-    .wrist = &wrist,
-    .hand = &hand,
-  };
+  arm_t arm =
+      {
+          .base = &base,
+          .shoulder = &shoulder,
+          .elbow = &elbow,
+          .wrist = &wrist,
+          .hand = &hand,
+      };
   arm_init(&arm);
   joint_set_angle(arm.shoulder, 70);
-  //USBD_CDC_init();
+  // USBD_CDC_init();
 
   /* USER CODE END 2 */
 
@@ -141,12 +144,12 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    //joint_set_angle(arm.elbow, 20);
-    arm_set_angles(&arm,USB_RX_Buf);
-    //HAL_Delay(1000);
-    //joint_set_angle(arm.elbow, 120);
+    // joint_set_angle(arm.elbow, 20);
+    arm_set_angles(&arm, USB_RX_Buf);
+    // HAL_Delay(1000);
+    // joint_set_angle(arm.elbow, 120);
     HAL_Delay(1);
-    
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -155,23 +158,23 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSE;
+   * in the RCC_OscInitTypeDef structure.
+   */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -187,9 +190,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
@@ -202,10 +204,10 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief TIM3 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief TIM3 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_TIM3_Init(void)
 {
 
@@ -259,14 +261,13 @@ static void MX_TIM3_Init(void)
 
   /* USER CODE END TIM3_Init 2 */
   HAL_TIM_MspPostInit(&htim3);
-
 }
 
 /**
-  * @brief TIM5 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief TIM5 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_TIM5_Init(void)
 {
 
@@ -312,26 +313,25 @@ static void MX_TIM5_Init(void)
 
   /* USER CODE END TIM5_Init 2 */
   HAL_TIM_MspPostInit(&htim5);
-
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void)
 {
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
@@ -339,9 +339,9 @@ static void MX_GPIO_Init(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -353,14 +353,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
--- Robot Arm/arm_test.py
+++ Robot Arm/arm_test.py
@@ -1,3 +1,6 @@
+from pynput.mouse import Listener, Button
+import threading
+import keyboard
 import struct
 import serial
 import time
@@ -25,18 +28,23 @@ class angles_t:
         return "base: " + str(self.base) + "\nshoulder: " + str(self.shoulder) + "\nelbow: " + str(self.elbow) + "\nwrist: " + str(self.wrist) + "\nhand: " + str(self.hand)
 
 
-min_angles = angles_t(base=20, shoulder=20, elbow=20, wrist=20, hand=20)
-max_angles = angles_t(base=100, shoulder=100, elbow=100, wrist=100, hand=100)
+min_angles = angles_t(base=20, shoulder=50, elbow=20, wrist=20, hand=0)
+max_angles = angles_t(base=180, shoulder=120, elbow=180, wrist=180, hand=90)
+
+start_angles = angles_t(base=50, shoulder=70, elbow=50, wrist=50, hand=90)
 
 
 class RobotArm:
 
     def __init__(self, port, min_angles, max_angles):
+        self.movable = False
         self.min_angles = min_angles
         self.max_angles = max_angles
-        self.curr_angles = min_angles
+        self.curr_angles = start_angles
         self.ser = serial.Serial(baudrate=115200, port=port)
+        self.send_angles()
 
+    # Make sure angles are valid
     def check_angles(self, angles: angles_t):
         def check_angle(angle, min_angle, max_angle):
             if (angle < min_angle):
@@ -56,6 +64,7 @@ class RobotArm:
             angles.hand, self.min_angles.hand, self.max_angles.hand)
         return angles
 
+    # send the angles over serial to robot stm32
     def send_angles(self, base=None, shoulder=None, elbow=None, wrist=None, hand=None):
         try:
             if (base):
@@ -70,16 +79,98 @@ class RobotArm:
                 self.curr_angles.hand = hand
             self.curr_angles = self.check_angles(self.curr_angles)
             angles = list(self.curr_angles)
+            angles = [int(i) for i in angles]
             for angle in angles:
                 if angle < 0:
                     raise ValueError("Negative numbers are not allowed")
             data = struct.pack('%sf' % len(angles), *angles)
-            self.ser.write(data)  # Assuming `ser` is defined elsewhere
+            if (self.movable):
+                self.ser.write(data)  # Assuming `ser` is defined elsewhere
         except Exception as e:
             print(e)
 
 
-arm = RobotArm(port="COM15", min_angles=min_angles, max_angles=max_angles)
+arm = RobotArm(port="COM5", min_angles=min_angles, max_angles=max_angles)
+
+last = None
+
+
+def on_move(x, y):
+    global arm
+    global last
+    if (not last):
+        last = x, y
+    dx = (x - last[0])
+    dy = (y - last[1])
+    last = x, y
+    target_base = arm.curr_angles.base - dx/3
+    target_shoulder = arm.curr_angles.shoulder - dy/8
+    if (target_base < arm.min_angles.base):
+        target_base = arm.min_angles.base
+    if (target_base > arm.max_angles.base):
+        target_base = arm.max_angles.base
+    if (target_shoulder < arm.min_angles.shoulder):
+        target_shoulder = arm.min_angles.shoulder
+    if (target_shoulder > arm.max_angles.shoulder):
+        target_shoulder = arm.max_angles.shoulder
+    # print(target_base, target_shoulder)
+    arm.send_angles(base=target_base)
+    arm.send_angles(shoulder=target_shoulder)
+
+    # print('Mouse moved to ({0}, {1})'.format(x, y))
+
+
+grab = -1
+
+
+def on_click(x, y, button, pressed):
+    global arm
+    global grab
+    if (button == Button.left and pressed):
+        grab = grab * -1
+        arm.send_angles(hand=50 + 40*grab)
+    if pressed:
+        print('Mouse clicked at ({0}, {1}) with {2} state {3}'.format(
+            x, y, button, pressed))
+
+
+start_wrist = None
+
+
+def on_scroll(x, y, dx, dy):
+    print(arm.curr_angles.elbow)
+    arm.send_angles(elbow=arm.curr_angles.elbow + dy*5)
+    # print('Mouse scrolled at ({0}, {1}) ({2}, {3})'.format(x, y, dx, dy))
+
+
+def keyboard_thread_func():
+    try:
+        while (1):
+            if (keyboard.is_pressed('z')):
+                if (not arm.movable):
+                    arm.movable = True
+            if (keyboard.is_pressed('x')):
+                if (arm.movable):
+                    arm.movable = False
+            if (keyboard.is_pressed('c')):
+                arm.send_angles(wrist=arm.curr_angles.wrist + 5)
+            if (keyboard.is_pressed('v')):
+                arm.send_angles(wrist=arm.curr_angles.wrist - 5)
+    except KeyboardInterrupt:
+        return
+
+
+keyboard_thread = threading.Thread(target=keyboard_thread_func)
+# Collect events until released
+with Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as listener:
+    try:
+        arm.movable = False
+        keyboard_thread.start()
+
+        keyboard_thread.join()
+        listener.join()
+    except KeyboardInterrupt:
+        pass
 
 
 def example_1():
@@ -94,13 +185,11 @@ def example_1():
 def example_2():
     arm.send_angles(base=80, shoulder=80, elbow=100, wrist=10, hand=10)
     time.sleep(1)
-    arm.send_angles(elbow=70)
+    arm.send_angles(hand=70)
     time.sleep(1)
-    arm.send_angles(elbow=10)
+    arm.send_angles(hand=10)
     time.sleep(1)
-    arm.send_angles(elbow=50)
+    arm.send_angles(hand=50)
     time.sleep(1)
-    arm.send_angles(elbow=90)
+    arm.send_angles(hand=90)
     time.sleep(1)
-
-example_2()
